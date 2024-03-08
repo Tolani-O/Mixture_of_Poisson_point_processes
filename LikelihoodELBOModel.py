Crucial bug fix for neuron factor access creation, and likelihood computation.
neuron factor access was not properly applied, and posterior weight matrices were not detached before computation

LikelihoodELBOModel.py
@@ -169,24 +169,22 @@ class LikelihoodELBOModel(nn.Module):
 
         return warped_factors
 
-
-    def compute_log_elbo(self, Y, neuron_factor_access, warped_factors): # and first 2 entropy terms
+    def compute_log_elbo(self, Y, neuron_factor_access, warped_factors):  # and first 2 entropy terms
         # Weight Matrices
 
         # warped_factors # L x T x M x N x R x C
         # Y # K x T x R x C
         # Y_times_N_matrix  # K x L x T x M x N x R x C
         # sum_Y_times_N_matrix  # K x L x M x N x C
-        # neuron_factor_access  #  C x K x L
         Y_times_N_matrix = torch.einsum('ktrc,ckl,ltmnrc->kltmnrc', Y, neuron_factor_access, warped_factors)
         sum_Y_times_N_matrix = torch.sum(Y_times_N_matrix, dim=(2, 5))
         exp_N_matrix = torch.exp(warped_factors)
         # sum_Y_term # K x C
         # logterm1  # K x C x L
         # logterm2  # L x M x N x C
-        sum_Y_term = torch.sum(Y, dim=(1,2)) # K x C
-        logterm1 = sum_Y_term[:,:,None] + F.softplus(self.alpha)[None,None,:]
-        logterm2 = self.dt * torch.sum(exp_N_matrix, dim=(1, 4)) + F.softplus(self.theta)[:,None,None,None]
+        sum_Y_term = torch.sum(Y, dim=(1, 2))  # K x C
+        logterm1 = sum_Y_term[:, :, None] + F.softplus(self.alpha)[None, None, :]
+        logterm2 = self.dt * torch.sum(exp_N_matrix, dim=(1, 4)) + F.softplus(self.theta)[:, None, None, None]
         # logterm # K x L x M x N x C
         logterm = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, torch.log(logterm2))
         # alphalogtheta # 1 x L x 1 x 1 x 1
@@ -194,7 +192,7 @@ class LikelihoodELBOModel(nn.Module):
         # sum_Y_times_logalpha  # K x C x L
         sum_Y_times_logalpha = torch.einsum('kc,ckl,l->klc', sum_Y_term, neuron_factor_access, torch.log(F.softplus(self.alpha)))
         # sum_Y_times_logalpha # K x L x 1 x 1 x C
-        sum_Y_times_logalpha = sum_Y_times_logalpha[:,:,None,None,:]
+        sum_Y_times_logalpha = sum_Y_times_logalpha[:, :, None, None, :]
         # logpi # 1 x L x 1 x 1 x 1
         logpi_expand = torch.log(F.softmax(torch.cat([torch.zeros(1), self.pi]), dim=0)).unsqueeze(0).unsqueeze(2).unsqueeze(3).unsqueeze(4)
         alpha_expand = F.softplus(self.alpha).unsqueeze(0).unsqueeze(2).unsqueeze(3).unsqueeze(4)
@@ -210,19 +208,25 @@ class LikelihoodELBOModel(nn.Module):
         exp_sum_logsumexp_tensor_reshape = exp_sum_logsumexp_tensor.reshape(-1, W_CMNK_tensor.shape[-1])
 
         # W_C_tensor # 1 x 1 x M x N x C
-        W_C_tensor = F.softmax(exp_sum_logsumexp_tensor_reshape, dim=0).reshape(exp_sum_logsumexp_tensor.shape)[None,None,:,:,:]
-        self.W_C_tensor = W_C_tensor.detach()
+        W_C_tensor = F.softmax(exp_sum_logsumexp_tensor_reshape, dim=0).reshape(exp_sum_logsumexp_tensor.shape)[None, None, :, :, :]
+        W_C_tensor = W_C_tensor.detach()
+        self.W_C_tensor = W_C_tensor
 
         # W_tensor # K x L x M x N x C
-        W_tensor = (W_CMNK_tensor * W_C_tensor).detach()
+        # neuron_factor_access  #  C x K x L
+        W_tensor = (torch.permute(neuron_factor_access, (1, 2, 0)).unsqueeze(2).unsqueeze(3) *
+                    W_CMNK_tensor * W_C_tensor).detach()
 
         # A_tensor # K x L x M x N x C
-        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1/logterm2)
+        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1 / logterm2).detach()
+        B_tensor = (torch.permute(torch.digamma(logterm1), (0, 2, 1))[:, :, None, None, :] -
+                    torch.log(logterm2[None, :, :, :, :])).detach()
 
         # Liklelihood Terms
-        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None,:,:,:,:] - torch.lgamma(alpha_expand) + alpha_expand *
-                           (torch.log(theta_expand) + torch.permute(torch.digamma(logterm1), (0, 2, 1))[:,:,None,None,:] -
-                            torch.log(logterm2[None,:,:,:,:])) + logpi_expand)
+        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None, :, :, :, :] - torch.lgamma(alpha_expand) +
+                     alpha_expand * (torch.log(theta_expand) + B_tensor) + logpi_expand)
+
+        # elbo_term # K x L x M x N x C
         elbo_term = torch.sum(W_tensor * elbo_term)
 
         return elbo_term

LikelihoodModel.py
@@ -136,23 +136,22 @@ class LikelihoodModel(nn.Module):
         return warped_factors
 
 
-    def compute_log_elbo(self, Y, neuron_factor_access, warped_factors): # and first 2 entropy terms
+    def compute_log_elbo(self, Y, neuron_factor_access, warped_factors):  # and first 2 entropy terms
         # Weight Matrices
 
         # warped_factors # L x T x M x N x R x C
         # Y # K x T x R x C
         # Y_times_N_matrix  # K x L x T x M x N x R x C
         # sum_Y_times_N_matrix  # K x L x M x N x C
-        # neuron_factor_access  #  C x K x L
         Y_times_N_matrix = torch.einsum('ktrc,ckl,ltmnrc->kltmnrc', Y, neuron_factor_access, warped_factors)
         sum_Y_times_N_matrix = torch.sum(Y_times_N_matrix, dim=(2, 5))
         exp_N_matrix = torch.exp(warped_factors)
         # sum_Y_term # K x C
         # logterm1  # K x C x L
         # logterm2  # L x M x N x C
-        sum_Y_term = torch.sum(Y, dim=(1,2)) # K x C
-        logterm1 = sum_Y_term[:,:,None] + F.softplus(self.alpha)[None,None,:]
-        logterm2 = self.dt * torch.sum(exp_N_matrix, dim=(1, 4)) + F.softplus(self.theta)[:,None,None,None]
+        sum_Y_term = torch.sum(Y, dim=(1, 2))  # K x C
+        logterm1 = sum_Y_term[:, :, None] + F.softplus(self.alpha)[None, None, :]
+        logterm2 = self.dt * torch.sum(exp_N_matrix, dim=(1, 4)) + F.softplus(self.theta)[:, None, None, None]
         # logterm # K x L x M x N x C
         logterm = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, torch.log(logterm2))
         # alphalogtheta # 1 x L x 1 x 1 x 1
@@ -160,7 +159,7 @@ class LikelihoodModel(nn.Module):
         # sum_Y_times_logalpha  # K x C x L
         sum_Y_times_logalpha = torch.einsum('kc,ckl,l->klc', sum_Y_term, neuron_factor_access, torch.log(F.softplus(self.alpha)))
         # sum_Y_times_logalpha # K x L x 1 x 1 x C
-        sum_Y_times_logalpha = sum_Y_times_logalpha[:,:,None,None,:]
+        sum_Y_times_logalpha = sum_Y_times_logalpha[:, :, None, None, :]
         # logpi # 1 x L x 1 x 1 x 1
         logpi_expand = torch.log(F.softmax(torch.cat([torch.zeros(1), self.pi]), dim=0)).unsqueeze(0).unsqueeze(2).unsqueeze(3).unsqueeze(4)
         alpha_expand = F.softplus(self.alpha).unsqueeze(0).unsqueeze(2).unsqueeze(3).unsqueeze(4)
@@ -173,15 +172,19 @@ class LikelihoodModel(nn.Module):
         W_CMNK_tensor = F.softmax(U_tensor, dim=1)
 
         # W_tensor # K x L x M x N x C
-        W_tensor = W_CMNK_tensor.detach()
+        # neuron_factor_access  #  C x K x L
+        W_tensor = (torch.permute(neuron_factor_access, (1, 2, 0)).unsqueeze(2).unsqueeze(3) * W_CMNK_tensor).detach()
 
         # A_tensor # K x L x M x N x C
-        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1/logterm2)
+        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1 / logterm2).detach()
+        B_tensor = (torch.permute(torch.digamma(logterm1), (0, 2, 1))[:, :, None, None, :] -
+                    torch.log(logterm2[None, :, :, :, :])).detach()
 
         # Liklelihood Terms
-        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None,:,:,:,:] - torch.lgamma(alpha_expand) + alpha_expand *
-                           (torch.log(theta_expand) + torch.permute(torch.digamma(logterm1), (0, 2, 1))[:,:,None,None,:] -
-                            torch.log(logterm2[None,:,:,:,:])) + logpi_expand)
+        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None, :, :, :, :] - torch.lgamma(alpha_expand) +
+                     alpha_expand * (torch.log(theta_expand) + B_tensor) + logpi_expand)
+
+        # elbo_term # K x L x M x N x C
         elbo_term = torch.sum(W_tensor * elbo_term)
 
         return elbo_term
@@ -189,12 +192,11 @@ class LikelihoodModel(nn.Module):
 
     def compute_log_elbo_peak_times(self, Y, neuron_factor_access, warped_factors):  # and first 2 entropy terms
         # Weight Matrices
-        self.warped_factors = warped_factors
+
         # warped_factors # L x T x M x N x R x C
         # Y # K x T x R x C
         # Y_times_N_matrix  # K x L x T x M x N x R x C
         # sum_Y_times_N_matrix  # K x L x M x N x C
-        # neuron_factor_access  #  C x K x L
         Y_times_N_matrix = torch.einsum('ktrc,ckl,ltmnrc->kltmnrc', Y, neuron_factor_access, warped_factors)
         sum_Y_times_N_matrix = torch.sum(Y_times_N_matrix, dim=(2, 5))
         exp_N_matrix = torch.exp(warped_factors)
@@ -222,28 +224,31 @@ class LikelihoodModel(nn.Module):
         W_CMNK_tensor = F.softmax(U_tensor, dim=1)
 
         # W_tensor # K x L x M x N x C
-        W_tensor = W_CMNK_tensor.detach()
+        # neuron_factor_access  #  C x K x L
+        W_tensor = (torch.permute(neuron_factor_access, (1, 2, 0)).unsqueeze(2).unsqueeze(3) * W_CMNK_tensor).detach()
 
         # A_tensor # K x L x M x N x C
-        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1/logterm2)
+        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1 / logterm2).detach()
 
         # Liklelihood Terms
-        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None,:,:,:,:])
+        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None, :, :, :, :])
+
+        # elbo_term # K x L x M x N x C
         elbo_term = torch.sum(W_tensor * elbo_term)
 
         return elbo_term
 
 
-    def compute_log_elbo_factor_assignments(self, Y, neuron_factor_access, factor_assignment_train):  # and first 2 entropy terms
+    def compute_log_elbo_factor_assignments(self, Y, neuron_factor_access, warped_factors):  # and first 2 entropy terms
         # Weight Matrices
 
         # warped_factors # L x T x M x N x R x C
         # Y # K x T x R x C
         # Y_times_N_matrix  # K x L x T x M x N x R x C
         # sum_Y_times_N_matrix  # K x L x M x N x C
-        Y_times_N_matrix = torch.einsum('ktrc,ltmnrc->kltmnrc', Y, self.warped_factors)
+        Y_times_N_matrix = torch.einsum('ktrc,ckl,ltmnrc->kltmnrc', Y, neuron_factor_access, warped_factors)
         sum_Y_times_N_matrix = torch.sum(Y_times_N_matrix, dim=(2, 5))
-        exp_N_matrix = torch.exp(self.warped_factors)
+        exp_N_matrix = torch.exp(warped_factors)
         # sum_Y_term # K x C
         # logterm1  # K x C x L
         # logterm2  # L x M x N x C
@@ -256,56 +261,51 @@ class LikelihoodModel(nn.Module):
         alpha_expand = F.softplus(self.alpha).unsqueeze(0).unsqueeze(2).unsqueeze(3).unsqueeze(4)
         theta_expand = F.softplus(self.theta).unsqueeze(0).unsqueeze(2).unsqueeze(3).unsqueeze(4)
 
+        W_tensor = torch.permute(neuron_factor_access, (1, 2, 0)).unsqueeze(2).unsqueeze(3)
         # A_tensor # K x L x M x N x C
-        A_tensor = torch.einsum('kcl,lmnc->klmnc', logterm1, 1 / logterm2)
+        A_tensor = torch.einsum('kcl,ckl,lmnc->klmnc', logterm1, neuron_factor_access, 1 / logterm2).detach()
+        B_tensor = (torch.permute(torch.digamma(logterm1), (0, 2, 1))[:, :, None, None, :] -
+                    torch.log(logterm2[None, :, :, :, :])).detach()
 
         # Liklelihood Terms
-        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None, :, :, :, :] - torch.lgamma(alpha_expand) + alpha_expand *
-                     (torch.log(theta_expand) + torch.permute(torch.digamma(logterm1), (0, 2, 1))[:, :, None, None, :] -
-                      torch.log(logterm2[None, :, :, :, :])) + logpi_expand)
-        # neuron_factor_access  #  C x K x L
+        elbo_term = (sum_Y_times_N_matrix - A_tensor * logterm2[None, :, :, :, :] - torch.lgamma(alpha_expand) +
+                     alpha_expand * (torch.log(theta_expand) + B_tensor) + logpi_expand)
+
         # elbo_term # K x L x M x N x C
-        elbo_term = torch.permute(neuron_factor_access, (1,2,0)).unsqueeze(2).unsqueeze(3) * elbo_term
-        elbo_term = torch.permute(elbo_term.squeeze(), (2, 0, 1))
-        # elbo_term # C x K x L
-        # neuron_factor_assignment  # C x K x L
-        self.neuron_factor_assignment = torch.where(elbo_term == torch.max(elbo_term, dim=2, keepdim=True).values, 1, 0)
+        elbo_term = W_tensor * elbo_term
 
-        # #debugging
-        anp = neuron_factor_access.detach().numpy()
-        enp = elbo_term.detach().numpy()
-        nnp = self.neuron_factor_assignment.detach().numpy()
-
-        return self.neuron_factor_assignment, enp
+        return elbo_term
 
 
     def compute_offset_entropy_terms(self):  # last 2 entropy terms
         # Entropy1 Terms
         dim = self.config_peak_offset_stdevs.shape[0]
 
-        Sigma1 = torch.diag(F.softplus(self.config_peak_offset_stdevs)) @ torch.diag(
-            F.softplus(self.config_peak_offset_stdevs)).t()
+        Sigma1 = torch.diag(F.softplus(self.config_peak_offset_stdevs)) @ torch.diag(F.softplus(self.config_peak_offset_stdevs)).t()
         det_Sigma1 = torch.linalg.det(Sigma1)
         inv_Sigma1 = torch.linalg.inv(Sigma1)
-        prod_term1 = torch.einsum('ncl,lj,ncj->nc', self.transformed_config_peak_offset_samples, inv_Sigma1,
-                                  self.transformed_config_peak_offset_samples)  # sum over l
+        prod_term1 = torch.einsum('ncl,lj,ncj->nc', self.transformed_config_peak_offset_samples, inv_Sigma1, self.transformed_config_peak_offset_samples)  # sum over l
         # entropy_term1  # N x C
         entropy_term1 = -0.5 * torch.sum(torch.log((2 * torch.pi) ** dim * det_Sigma1) + prod_term1)
 
         Sigma2 = self.trial_peak_offset_covar_ltri @ self.trial_peak_offset_covar_ltri.t()
         det_Sigma2 = torch.linalg.det(Sigma2)
         inv_Sigma2 = torch.linalg.inv(Sigma2)
-        prod_term2 = torch.einsum('mrcl,lj,mrcj->mrc', self.transformed_trial_peak_offset_samples, inv_Sigma2,
-                                  self.transformed_trial_peak_offset_samples)  # sum over l
+        prod_term2 = torch.einsum('mrcl,lj,mrcj->mrc', self.transformed_trial_peak_offset_samples, inv_Sigma2, self.transformed_trial_peak_offset_samples)  # sum over l
         # entropy_term2  # M x C
         entropy_term2 = -0.5 * torch.sum(torch.log((2 * torch.pi) ** dim * det_Sigma2) + prod_term2)
 
         entropy_term = entropy_term1 + entropy_term2
         return entropy_term
 
-    def compute_neuron_factor_assignment(self, Y, neuron_factor_access):
-        cluster_probs = self.compute_log_elbo_factor_assignments(Y, neuron_factor_access)
-        return cluster_probs
+
+    def compute_neuron_factor_assignment(self, Y, neuron_factor_access, warped_factors):
+        likelihoods = self.compute_log_elbo_factor_assignments(Y, neuron_factor_access, warped_factors)
+        # likelihoods # K x L x M x N x C
+        likelihoods = torch.permute(likelihoods.squeeze(), (2, 0, 1))
+        # likelihoods # C x K x L
+        self.neuron_factor_assignment = torch.where(likelihoods == torch.max(likelihoods, dim=2, keepdim=True).values, 1, 0)
+        return self.neuron_factor_assignment
 
 
     def forward(self, Y, neuron_factor_access):

main_em_torch.py
@@ -1,8 +1,10 @@
 import os
 import sys
+
 sys.path.append(os.path.abspath('.'))
 from src.EM_Torch.simulate_data_multitrial import DataAnalyzer
 from src.EM_Torch.LikelihoodELBOModel import LikelihoodELBOModel
+from src.EM_Torch.LikelihoodModel import LikelihoodModel
 from src.EM_Torch.general_functions import load_model_checkpoint, softplus, plot_spikes, \
     plot_intensity_and_latents, create_relevant_files, get_parser, plot_outputs, \
     write_log_and_model, write_losses, plot_losses, CustomDataset
@@ -11,7 +13,6 @@ import time
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-
 
 args = get_parser().parse_args()
 if args.param_seed == '':
@@ -22,7 +23,7 @@ args.data_seed = np.random.randint(0, 2 ** 32 - 1)
 # args.n_configs = 5  # C
 # args.n_trial_samples = 4  # M
 # args.n_config_samples = 4  # N
-# args.K = 10  # K
+# args.K = 3  # K
 
 # args.folder_name = ''
 # args.load = True
@@ -46,70 +47,66 @@ if args.load:
     output_dir = os.path.join(output_dir, args.folder_name)
     model, output_str, data = load_model_checkpoint(output_dir, args.load_epoch)
     # Training data
-    Y_train, stim_time, factor_access_train, intensities_train = data.sample_data(K=args.K, A=args.A,
-                                                                                  n_configs=args.n_configs,
-                                                                                  n_trials=args.n_trials)
+    Y_train, stim_time, factor_access_train = data.sample_data(K=args.K, A=args.A, n_configs=args.n_configs, n_trials=args.n_trials)
+    intensities_train, factor_assignment_train, config_offsets_train, trial_peak_offsets_train = data.get_data_ground_truth()
     # Validation data
-    Y_test, _, factor_access_test, intensities_test = data.sample_data(K=args.K, A=args.A, n_configs=args.n_configs,
-                                                                       n_trials=args.n_trials)
+    Y_test, _, factor_access_test = data.sample_data(K=args.K, A=args.A, n_configs=args.n_configs, n_trials=args.n_trials)
+    intensities_test, factor_assignment_test, config_offsets_test, trial_peak_offsets_test = data.get_data_ground_truth()
     output_str_split = output_str.split(':')
     true_ELBO_train = float(output_str_split[3].split(',')[0].strip())
     true_ELBO_test = float(output_str_split[4].split('\n')[0].strip())
-    start_epoch = args.load_epoch+1
+    start_epoch = args.load_epoch + 1
 else:
     # Ground truth data
     data = DataAnalyzer().initialize(A=args.A, intensity_mltply=args.intensity_mltply, intensity_bias=args.intensity_bias)
     # Training data
-    Y_train, stim_time, factor_access_train, intensities_train = data.sample_data(K=args.K, A=args.A,
-                                                                                  n_configs=args.n_configs,
-                                                                                  n_trials=args.n_trials)
+    Y_train, stim_time, factor_access_train = data.sample_data(K=args.K, A=args.A, n_configs=args.n_configs, n_trials=args.n_trials)
+    intensities_train, factor_assignment_train, config_offsets_train, trial_offsets_train = data.get_data_ground_truth()
     # Validation data
-    Y_test, _, factor_access_test, intensities_test = data.sample_data(K=args.K, A=args.A, n_configs=args.n_configs,
-                                                                       n_trials=args.n_trials)
+    Y_test, _, factor_access_test = data.sample_data(K=args.K, A=args.A, n_configs=args.n_configs, n_trials=args.n_trials)
+    intensities_test, factor_assignment_test, config_offsets_test, trial_offsets_test = data.get_data_ground_truth()
 
-    true_likelihood_train = data.compute_log_likelihood(Y_train, intensities_train)
-    true_likelihood_test = data.compute_log_likelihood(Y_test, intensities_test)
+    # initialize the likelihood model with ground truth params
+    with torch.no_grad():
+        true_model = LikelihoodModel(stim_time)
+        true_model.init_params(torch.tensor(data.beta).float(), torch.tensor(data.alpha).float(),
+                               torch.tensor(data.theta).float(), torch.tensor(data.pi).float(),
+                               torch.tensor(data.config_peak_offset_stdevs).float(),
+                               torch.tensor(data.trial_peak_offset_covar_ltri).float(),
+                               args.n_configs, args.K)
+        true_model.init_ground_truth(torch.tensor(config_offsets_train).float(), torch.tensor(trial_offsets_train).float())
+        likelihood_term_train, entropy_term_train = true_model.forward(torch.tensor(Y_train), torch.tensor(factor_access_train))
+        true_ELBO_train = likelihood_term_train + entropy_term_train
+        true_model.init_ground_truth(torch.tensor(config_offsets_test).float(), torch.tensor(trial_offsets_test).float())
+        likelihood_term_test, entropy_term_test = true_model.forward(torch.tensor(Y_test), torch.tensor(factor_access_test))
+        true_ELBO_test = likelihood_term_test + entropy_term_test
 
-    # initialize the model with training data and ground truth params
+    start_epoch = 0
+
+    num_factors = data.beta.shape[0]
+
     model = LikelihoodELBOModel(stim_time, args.n_trial_samples, args.n_config_samples)
     model.init_ground_truth(data.beta.shape[0],
                             torch.tensor(data.beta).float(), torch.tensor(data.alpha).float(),
                             torch.tensor(data.theta).float(), torch.tensor(data.pi).float(),
                             torch.tensor(data.config_peak_offset_stdevs).float(),
                             torch.tensor(data.trial_peak_offset_covar_ltri).float())
-
-    model.eval()
-    with torch.no_grad():
-        warped_factors = model.warp_all_latent_factors_for_all_trials(args.n_configs, args.n_trials)
-        likelihood_term_train = model.compute_log_elbo(torch.tensor(Y_train), torch.tensor(factor_access_train),
-                                                       warped_factors)
-        entropy_term_train = model.compute_offset_entropy_terms()
-        likelihood_term_test = model.compute_log_elbo(torch.tensor(Y_test), torch.tensor(factor_access_test),
-                                                      warped_factors)
-        entropy_term_test = model.compute_offset_entropy_terms()
-        true_ELBO_train = likelihood_term_train + entropy_term_train
-        true_ELBO_test = likelihood_term_test + entropy_term_test
-
-    start_epoch = 0
-
-    num_factors = data.beta.shape[0]
     # model.init_ground_truth(num_factors, torch.zeros_like(torch.tensor(data.beta)).float())
-    model.init_ground_truth(num_factors, torch.tensor(data.beta).float())
+    # model.init_ground_truth(num_factors, torch.tensor(data.beta).float())
     # la = int(num_factors/args.A)
     # factor_indcs = [i*la for i in range(args.A)]
     # model.init_from_data(Y=torch.tensor(Y_train).float(), neuron_factor_access=torch.tensor(factor_access_train).float(),
     #                      factor_indcs=factor_indcs)
     # model.init_random(num_factors)
 
-    args.folder_name = (f'{args.param_seed}_dataSeed{args.data_seed}_K{args.K}_R{args.n_trials}_A{args.A}_C{args.n_configs}'
-                        f'_R{args.n_trials}_tauBeta{args.tau_beta}_tauSigma1{args.tau_sigma1}_tauSigma2{args.tau_sigma2}'
-                        f'_iters{args.num_epochs}_BatchSize{args.batch_size}_lr{args.lr}_notes-{args.notes}')
+    args.folder_name = (
+        f'{args.param_seed}_dataSeed{args.data_seed}_K{args.K}_R{args.n_trials}_A{args.A}_C{args.n_configs}'
+        f'_R{args.n_trials}_tauBeta{args.tau_beta}_tauSigma1{args.tau_sigma1}_tauSigma2{args.tau_sigma2}'
+        f'_iters{args.num_epochs}_BatchSize{args.batch_size}_lr{args.lr}_notes-{args.notes}')
     output_dir = os.path.join(output_dir, args.folder_name)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     output_str = (
-        f"True likelihood Training: {true_likelihood_train},\n"
-        f"True likelihood Test: {true_likelihood_test},\n"
         f"True ELBO Training: {true_ELBO_train},\n"
         f"True ELBO Test: {true_ELBO_test}\n\n")
     create_relevant_files(output_dir, args, output_str, data)
@@ -169,8 +166,7 @@ if __name__ == "__main__":
                 stdevs_mses.append(F.mse_loss(model.config_peak_offset_stdevs, torch.tensor(data.config_peak_offset_stdevs)).item())
                 ltri_mses.append(F.mse_loss(model.trial_peak_offset_covar_ltri, torch.tensor(data.trial_peak_offset_covar_ltri)).item())
 
-                likelihood_term = model.compute_log_elbo(torch.tensor(Y_test), torch.tensor(factor_access_test),
-                                                         warped_factors)
+                likelihood_term = model.compute_log_elbo(torch.tensor(Y_test), torch.tensor(factor_access_test), warped_factors)
                 entropy_term = model.compute_offset_entropy_terms()
                 losses_test.append((likelihood_term + entropy_term + penalty_term).item())
                 log_likelihoods_test.append((likelihood_term + entropy_term).item())
@@ -186,7 +182,7 @@ if __name__ == "__main__":
             with torch.no_grad():
                 smoothness_budget_constrained = F.softmax(model.smoothness_budget, dim=0).numpy()
                 latent_factors = F.softplus(model.beta).numpy()
-                warped_factors = None # model.warp_all_latent_factors_for_all_trials(args.n_configs, args.n_trials).numpy()
+                warped_factors = None  # model.warp_all_latent_factors_for_all_trials(args.n_configs, args.n_trials).numpy()
             output_str = (
                 f"Epoch: {epoch:2d}, Elapsed Time: {elapsed_time / 60:.2f} mins, Total Time: {total_time / (60 * 60):.2f} hrs,\n"
                 f"Loss train: {cur_loss_train:.5f}, Log Likelihood train: {cur_log_likelihood_train:.5f},\n"

simulate_data_multitrial.py
@@ -48,11 +48,10 @@ class DataAnalyzer:
         self.theta = np.random.normal(size=n_factors)
         self.pi = np.random.normal(size=n_factors-1)
         self.config_peak_offset_stdevs = np.random.normal(size=2 * n_factors)
-        bounds = 0.05
         matrix = np.tril(np.random.normal(size=(2 * n_factors, 2 * n_factors)))
         # Ensure diagonal elements are positive
         for i in range(min(matrix.shape)):
-            matrix[i, i] += softplus(matrix[i, i])
+            matrix[i, i] += 2*softplus(matrix[i, i])
         # Make it a learnable parameter
         self.trial_peak_offset_covar_ltri = matrix
         # solely to check if the covariance matrix is positive semi-definite
@@ -100,16 +99,17 @@ class DataAnalyzer:
 
     def generate_neuron_gains_factor_assignments_condition_assignment_and_factor_access(self, num_neurons, num_areas, num_conditions):
 
-        num_factors = len(self.pi)+1
+        num_factors = len(self.config_peak_offset_stdevs)//2
+        factors_per_area = num_factors//num_areas
         ratio = softmax(np.hstack([np.zeros(1), self.pi]), axis=0)
         neuron_factor_assignments = np.random.choice(num_factors, num_neurons*num_conditions, p=ratio).reshape(num_conditions, -1)
         neuron_factor_access = np.zeros((num_conditions, num_neurons, num_factors))
         for a in range(num_areas):
-            area_start_indx = a * (num_factors//2)
-            neuron_factor_access[((area_start_indx<=neuron_factor_assignments)&((area_start_indx+2)>=neuron_factor_assignments)), (3*a):(3*a+3)] = 1
-        neuron_gains = (np.random.gamma(softplus(self.alpha[neuron_factor_assignments.flatten()]),
-                                        softplus(self.theta[neuron_factor_assignments.flatten()]))
-                        .reshape(neuron_factor_assignments.shape))
+            area_start_indx = a*factors_per_area
+            neuron_factor_access[((area_start_indx<=neuron_factor_assignments)&((area_start_indx+factors_per_area)>neuron_factor_assignments)),
+                                 area_start_indx:(area_start_indx+factors_per_area)] = 1
+        neuron_gains = np.random.gamma(softplus(self.alpha[neuron_factor_assignments]),
+                                       softplus(self.theta[neuron_factor_assignments]))
         self.neuron_gains = neuron_gains
         self.neuron_factor_assignments = neuron_factor_assignments
         self.neuron_factor_access = neuron_factor_access
@@ -224,7 +224,16 @@ class DataAnalyzer:
         trial_warped_factors = self.warp_all_latent_factors_for_all_trials().squeeze()
         self.generate_neuron_gains_factor_assignments_condition_assignment_and_factor_access(K, A, n_configs)
         self.generate_spike_trains(trial_warped_factors)
-        return self.Y, self.time, self.neuron_factor_access, self.neuron_intensities
+        indcs = np.indices(self.neuron_factor_assignments.shape)
+        neuron_factor_assignments = np.zeros((n_configs, K, n_factors))
+        neuron_factor_assignments[indcs[0].flatten(), indcs[1].flatten(), self.neuron_factor_assignments.flatten()] = 1
+        self.neuron_factor_assignments = neuron_factor_assignments
+        return self.Y, self.time, self.neuron_factor_access
+
+
+    def get_data_ground_truth(self):
+        return (self.neuron_intensities, self.neuron_factor_assignments, self.transformed_config_peak_offset_samples,
+                self.transformed_trial_peak_offset_samples)
 
     def compute_log_likelihood(self, Y, neuron_intensities):
         likelihood = np.sum(np.log(neuron_intensities) * Y - neuron_intensities * self.dt)
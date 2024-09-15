Corrected initialization for gamma parameters (alpha and theta). Corrected computation of ELBO.

simulate_data_multitrial.py
@@ -66,8 +66,8 @@ class DataAnalyzer:
         # self.trial_peak_offset_covar_ltri = 1e-10 * np.eye(2 * n_factors)
         latent_factors = self.generate_latent_factors(intensity_type, intensity_mltply, intensity_bias)
         latent_factors = np.vstack([latent_factors] * A)
-        latent_factors = latent_factors / np.sum(latent_factors, axis=1, keepdims=True)
         self.beta = np.log(latent_factors)
+        # latent_factors = latent_factors / np.sum(latent_factors, axis=1, keepdims=True)
         return self
 
     def generate_latent_factors(self, intensity_type, intensity_mltply, intensity_bias):
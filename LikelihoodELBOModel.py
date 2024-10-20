Minor fixes with second derivative

LikelihoodELBOModel.py
@@ -34,7 +34,7 @@ class LikelihoodELBOModel(nn.Module):
         self.n_trial_samples = n_trial_samples
         self.n_configs = n_configs
         self.n_trials = n_trials
-        Delta2 = create_second_diff_matrix(T, dt)
+        Delta2 = create_second_diff_matrix(T)
         self.Delta2TDelta2 = torch.tensor(Delta2.T @ Delta2)  # T x T # tikhonov regularization
 
         # Storage for use in the forward pass
@@ -456,7 +456,7 @@ class LikelihoodELBOModel(nn.Module):
         inv_Sigma = self.trial_peak_offset_proposal_sds**(-2)
         prod_term = torch.sum(trial_peak_offsets * inv_Sigma.unsqueeze(0), dim=-1).permute(2, 1, 0) # sum over l
         entropy_term = 0.5 * (n_dims * torch.log(torch.tensor(2 * torch.pi)) + torch.log(det_Sigma.t().unsqueeze(-1)) + prod_term)
-        return entropy_term # C x R x N
+        return entropy_term  # C x R x N
 
 
     def compute_penalty_terms(self, tau_beta, tau_config, tau_sigma, tau_sd):
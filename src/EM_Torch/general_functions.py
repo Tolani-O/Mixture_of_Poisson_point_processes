Minor fixes with second derivative

src/EM_Torch/general_functions.py
@@ -93,18 +93,17 @@ def create_first_diff_matrix(P):
     return D
 
 
-def create_second_diff_matrix(P, dt):
+def create_second_diff_matrix(P):
     D = np.zeros((P-2, P))
     # fill the main diagonal with 1s
     np.fill_diagonal(D, 1)
     # fill the subdiagonal and superdiagonal with -2s
     np.fill_diagonal(D[:, 2:], 1)
     np.fill_diagonal(D[:, 1:], -2)
-    D = D/(dt**2)
     # first row is a forward difference
-    s0 = [2, -5, 4, -1]
-    D0 = np.concatenate((s0, np.zeros(P-4)))/(dt**3)
-    D = np.vstack((D0, D, np.flip(D0)))
+    s0 = [1, -2, 1]
+    D0 = np.concatenate((s0, np.zeros(P-len(s0))))
+    D = P**2 * np.vstack((D0, D, np.flip(D0)))
     return D
 
 
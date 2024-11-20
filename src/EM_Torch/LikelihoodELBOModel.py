Fixed problem with dipping factors: there were round off errors in the time computation
It was cause by two things:
1. There were issues with the floating point precision used to create new tensors that was making things round improperly. Particularly when you do ceil and floor, you want things rounded to get the proper values. Lots of x.9999999 being floored to x, instead of (x+1), which would be the case if we round first.
2. The Allen API was not returning times that started at 0. I created a new time vector to correct this

src/EM_Torch/LikelihoodELBOModel.py
@@ -58,15 +58,15 @@ class LikelihoodELBOModel(nn.Module):
             weights = [weights] * len(temperature)
         assert len(temperature) == len(weights), "Temperature and weights must be the same length"
         self.temperature = torch.tensor(temperature)
-        self.weights = torch.tensor(weights, dtype=torch.float)
+        self.weights = torch.tensor(weights, dtype=torch.float64)
         self.time = torch.tensor(time)
         self.dt = torch.round(time[1] - time[0], decimals=3)
         T = time.shape[0]
-        self.peak1_left_landmarks = torch.searchsorted(self.time, torch.tensor(peak1_left_landmarks), side='left')-1
+        self.peak1_left_landmarks = torch.searchsorted(self.time, torch.tensor(peak1_left_landmarks), side='left')
         self.peak1_left_landmarks = torch.cat([self.peak1_left_landmarks] * n_areas)
         self.peak1_right_landmarks = torch.searchsorted(self.time, torch.tensor(peak1_right_landmarks), side='left')
         self.peak1_right_landmarks = torch.cat([self.peak1_right_landmarks] * n_areas)
-        self.peak2_left_landmarks = torch.searchsorted(self.time, torch.tensor(peak2_left_landmarks), side='left')-1
+        self.peak2_left_landmarks = torch.searchsorted(self.time, torch.tensor(peak2_left_landmarks), side='left')
         self.peak2_left_landmarks = torch.cat([self.peak2_left_landmarks] * n_areas)
         self.peak2_right_landmarks = torch.searchsorted(self.time, torch.tensor(peak2_right_landmarks), side='left')
         self.peak2_right_landmarks = torch.cat([self.peak2_right_landmarks] * n_areas)
@@ -364,10 +364,9 @@ class LikelihoodELBOModel(nn.Module):
         left_slope = (avg_peak_times - left_landmarks) / left_shifted_peak_times
         right_slope = (avg_peak_times - right_landmarks) / right_shifted_peak_times
         # left_shifted_time # 2AL x max_landmark_spread x N x R x C
-        left_shifted_time = torch.stack([torch.nn.functional.pad(self.time[left_landmarks_int[i]:(right_landmarks_int[i]+1)] - self.time[left_landmarks_int[i]],
-                                                     (0, max_landmark_spread - landmark_speads[i]),
-                                                     value=self.time[right_landmarks_int[i]] - self.time[left_landmarks_int[i]])
-                             for i in range(landmark_speads.shape[0])]).unsqueeze(2).unsqueeze(3).unsqueeze(4).expand_as(left_shifted_peak_times)
+        left_shifted_time = torch.stack([torch.nn.functional.pad(self.time[:landmark_speads[i]], (0, max_landmark_spread - landmark_speads[i]),
+                                                                 value=self.time[landmark_speads[i]-1])
+                                         for i in range(landmark_speads.shape[0])]).unsqueeze(2).unsqueeze(3).unsqueeze(4).expand_as(left_shifted_peak_times)
         slope_mask = (left_shifted_time <= left_shifted_peak_times).int()
         # warped_times # 2AL x max_landmark_spread x N x R x C
         warped_times = slope_mask * (left_shifted_time * left_slope + left_landmarks) + (1 - slope_mask) * ((left_shifted_time - left_shifted_peak_times) * right_slope + avg_peak_times)
@@ -377,11 +376,12 @@ class LikelihoodELBOModel(nn.Module):
     def compute_warped_factors(self, warped_times):
         factors = torch.cat([torch.exp(self.unnormalized_log_factors())] * 2, dim=0)
         # warped_time  # 2AL x len(landmark_spread) x N x R X C
-        warped_indices = warped_times / self.dt
+        dp = len(str(self.dt.item()).split('.')[1])
+        warped_indices = (warped_times / self.dt).round(decimals=dp)
         floor_warped_indices = torch.floor(warped_indices).int()
         ceil_warped_indices = torch.ceil(warped_indices).int()
-        ceil_weights = warped_indices - floor_warped_indices
-        floor_weights = 1 - ceil_weights
+        ceil_weights = (warped_indices - floor_warped_indices).round(decimals=dp)
+        floor_weights = (1 - ceil_weights).round(decimals=dp)
         floor_warped_factor = torch.stack([factors[i, floor_warped_indices[i]] for i in range(2*self.n_factors)])
         weighted_floor_warped_factor = floor_warped_factor * floor_weights
         ceil_warped_factor = torch.stack([factors[i, ceil_warped_indices[i]] for i in range(2*self.n_factors)])

src/EM_Torch/general_functions.py
@@ -68,7 +68,7 @@ class CustomDataset(Dataset):
         return self.Y[idx], self.neuron_factor_access[:, idx, :] # K x C x L
 
 
-def preprocess_input_data(Y, neuron_factor_access, time, mask_threshold=0):
+def preprocess_input_data(Y, neuron_factor_access, dt, mask_threshold=0):
     processed_inputs = {}
     # Y # K x T x R x C
     # neuron_factor_access # C x K x L
@@ -76,6 +76,7 @@ def preprocess_input_data(Y, neuron_factor_access, time, mask_threshold=0):
     Y_sum_rt = torch.sum(Y_sum_t, dim=-1)  # C x K
     empty_trials = torch.where(Y_sum_rt <= mask_threshold)
     neuron_factor_access[empty_trials[0], empty_trials[1]] = 0
+    time = torch.arange(Y.shape[1], device=Y.device) * dt
     processed_inputs['time'] = time
     processed_inputs['Y'] = Y
     processed_inputs['Y_sum_t'] = Y_sum_t
@@ -416,7 +417,6 @@ def plot_outputs(model, unique_regions, output_dir, folder, epoch, se_dict=None,
         plt.tight_layout()
         plt.savefig(os.path.join(warp_time_dir, f'warped_times_{epoch}.png'))
         plt.close()
-
 
         alpha = F.softplus(model.alpha).numpy()
         plt.figure(figsize=(10, 10))